79284d98225d4baac4f3addc467b97651d290841
RubenCordeiro <ruben.cordeiro.eng@gmail.com>
2015-02-19 01:10:54 +0000

    Add further optimizations

---
+++ js/ga.js
@@ -0,0 +1,2 @@
+window.onload=function(){!function(n,e){n.GoogleAnalyticsObject=e,n[e]=n[e]||function(){(n[e].q=n[e].q||[]).push(arguments)},n[e].l=1*new Date}(window,"ga"),ga("create","UA-XXXX-Y"),ga("send","pageview")};
+};
